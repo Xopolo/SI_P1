beb4409bcb77c6c5db90f9e53148a93bdaac98aa
Pablo Hernandez <phernandhy@alumnos.unex.es>
2021-10-31 19:46:18 +0100

    Initial Commit

---
+++ src/main/java/Individuos/IndividuoAckley.java
@@ -0,0 +1,20 @@
+package Individuos;
+
+import Comunes.Individuo;
+
+public class IndividuoAckley extends Individuo {
+
+    public IndividuoAckley(int tipo_mutacion) {
+        super(tipo_mutacion);
+    }
+
+    @Override
+    public double evaluar() {
+        return 0;
+    }
+
+    @Override
+    public void mutar() {
+
+    }
+}
